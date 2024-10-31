5d53a47da90bea54c18c1de0463dd570c15a6fde
LucasFJ-2023 <143094958+LucasFJ-2023@users.noreply.github.com>
2024-10-31 22:35:56 +0100

    Update app.py
    Update code

--- app.py
+++ app.py
@@ -14,41 +14,37 @@ def get_db_connection():
 #GET room_price by room_type and season
 @app.route('/rooms/<room_type>/<season>', methods=['GET'])
 def get_room_price(room_type, season):
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute("""
-            SELECT dayly_price FROM room_pricing WHERE room_type = ? AND season = ?
-        """, 
-        (room_type, season))
-        row = cur.fetchone()
-        conn.close()
-    
-        if get_room_price:
+        with sqlite3.connect(database_path) as conn:
+            cur = conn.cursor()
+            cur.execute("""
+                SELECT daily_price FROM rooms_pricing WHERE room_type = ? AND season = ?
+            """, 
+            (room_type, season))
+            result = cur.fetchone()
+
             return jsonify({
                  "room_type": room_type,
                  "season": season,
-                 "dayly_price": row("dayly_price")}), 200
-        else:
-             return jsonify({"error": "Price not found"}), 404
-
+                 "daily_price": result[0]}), 200
+        
 
 
 #Change price for a specific room in a specific season.
 @app.route('/rooms/<room_type>/<season>', methods=['PUT'])
 def update_room_price(room_type, season):
      data = request.get_json()
-     new_price =data.get("dayly_price")
+     new_price =data.get("daily_price")
 
      #Connect to database and change price
      conn = get_db_connection()
      cur = conn.cursor()
      cur.execute("""
-        UPDATE room_pricing SET dayly_price = ? WHERE room_type = ? AND season = ?
+        UPDATE rooms_pricing SET daily_price = ? WHERE room_type = ? AND season = ?
 """, (new_price, room_type, season))
      conn.commit()
 
      if cur.rowcount > 0:
-          response = jsonify({"message": "Dayly prices updated"}), 200
+          response = jsonify({"message": "Daily prices updated"}), 200
      else:
           response = jsonify({"error":"error"}), 404
      conn.close()
@@ -56,7 +52,27 @@ def update_room_price(room_type, season):
      return response
 
 
-app.run(debug=True, host='0.0.0.0', port=5002)
+
+#Send all data
+@app.route('/room-pricing/data', methods=["GET"])
+def get_bookings_data():
+    with sqlite3.connect('/app/data/datarooms.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM datarooms")
+        data = cur.fetchall()
+
+        #Check the response
+        if not data:
+            #response is empty
+            return "There was an error trying to retrieve price from a specific room!", 400
+        return data
+
+
+
+
+
+
+app.run(debug=True, host='0.0.0.0')
      
 
 # docker build -t kong_arthur_room_price .    -> Først skrives den her nede i terminalen
